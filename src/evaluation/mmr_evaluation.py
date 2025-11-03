2404e14c10e4a002d1cfa1364e7034e775809624
Jacob K. Autzen <119827786+JacobKautzen@users.noreply.github.com>
2025-11-03 14:36:34 +0100

    test push

---
+++ src/evaluation/mmr_evaluation.py
@@ -0,0 +1,12 @@
+#intra list diversity
+#can be illustrated in a graph (boxplot) if we compare different models
+def intra_list_diversity(recommended, item_similarity):
+
+
+
+#Coverage
+#vcan be illustrated in bar chart if multiple algorithms are evaluated
+def item_coverage(recommendations, all_items):
+
+
+#Novelty
