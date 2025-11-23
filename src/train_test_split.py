a6cde312f9ea0225f9ce9702618648971b7be431
Li-Thao-Phung <lphung23@student.aau.dk>
2025-11-23 11:45:50 +0100

    .

--- src/train_test_split.py
+++ src/train_test_split.py
@@ -3,92 +3,135 @@ import os
 import pandas as pd
 
 
-def split_ratings_dataset(
-    input_csv: str,
-    output_dir: str,
-    chunksize: int = None,
-    test_size:  float = 0.2,
-    random_state: int = 42
-):
-  # ensure output folder exsits 
-  os.makedirs(output_dir, exist_ok=True)
 
-  # Load dataset
-  df = pd.read_csv(input_csv, nrows=chunksize)
+def standardize_csv(
+    input_csv: str,
+    output_csv: str,
+    col_mapping: dict = None,
+    drop_columns: list = None,
+    nrows: int = None
+):
+
+  df = pd.read_csv(input_csv, nrows=nrows)
+
+  existing_mapping = {}
+
+  # rename according to mapping
+  for old,new in col_mapping.items():
+    if old in df.columns:
+      existing_mapping[old] = new
+
+  df.rename(columns=existing_mapping, inplace=True)
+
+
+  # drop unwanted columns
+  if drop_columns:
+    for col in drop_columns:
+      if col in df.columns:
+        df.drop(columns=col, inplace=True)
+
+
+  # save standardized CSV
+  os.makedirs(os.path.dirname(output_csv), exist_ok=True)
+  df.to_csv(output_csv, index=False)
+  print(f"Standardized CSV saved: {output_csv}")
+
+
+  return df
+
+
+
+def split_ratings(
+    ratings_df: pd.DataFrame,
+    output_dir: str,
+    dataset_name: str,
+    test_size: float = 0.2,
+    val_size: float = 0.2,
+    random_state: int = 42,
+    chunksize: int = None,
+):
+
+  os.makedirs(output_dir, exist_ok=True)
 
   # Split for each user
   train_list = []
   test_list = []
+  val_list = []
 
-  for user_id, group in df.groupby('userId'):
+  for user_id, group in ratings_df.groupby('userId'):
+
+    # split into train and test
     train_ratings, test_ratings = train_test_split(group, test_size=test_size, random_state=random_state)
+
+    # split intro train and validation
+    train_ratings, val_ratings = train_test_split(train_ratings, test_size=val_size, random_state=random_state)
+
     train_list.append(train_ratings)
+    val_list.append(val_ratings)
     test_list.append(test_ratings)
 
-    # Combine splits
-    train_df = pd.concat(train_list).reset_index(drop=True)
-    test_df = pd.concat(test_list).reset_index(drop=True)  
+  # Combine splits
+  train_df = pd.concat(train_list).reset_index(drop=True)
+  val_df = pd.concat(val_list).reset_index(drop=True)
+  test_df = pd.concat(test_list).reset_index(drop=True)
 
-    # Build output paths
-    train_file = os.path.join(output_dir, f"ratings_{chunksize}_train.csv")
-    test_file = os.path.join(output_dir, f"ratings_{chunksize}_test.csv")
+  # Build output paths
+  train_file = os.path.join(output_dir, f"{dataset_name}_ratings_{chunksize}_train.csv")
+  val_file = os.path.join(output_dir, f"{dataset_name}_ratings_{chunksize}_val.csv")
+  test_file = os.path.join(output_dir, f"{dataset_name}_ratings_{chunksize}_test.csv")
 
-    #save
-    train_df.to_csv(train_file, index=False)
-    test_df.to_csv(test_file, index=False)
+  #save
+  train_df.to_csv(train_file, index=False)
+  val_df.to_csv(val_file, index=False)
+  test_df.to_csv(test_file, index=False)
 
 
   print(
       f"Train/Test dataset generated.\n"
-      f"Rows: {len(df)} (train={len(train_df)}, test={len(test_df)})\n"
+      f"Rows: {len(ratings_df)} (train={len(train_df)},val={len(val_df)}, test={len(test_df)})\n"
       f"Saved to: {output_dir}"
   )
 
 
-# Parameters 
-CHUNKSIZE = 10000
+
+# Parameters
+CHUNKSIZE = 100000
 TEST_SIZE = 0.20
 
 
 #load dataset
 base_dir = os.path.dirname(os.path.abspath(__file__))
-input_csv = os.path.join(base_dir, "datasets", "ratings.csv")
-output_dir = os.path.join(base_dir, "datasets/mmr_data")
+input_rating_csv = os.path.join(base_dir, "datasets/MovieLens", "ratings.csv")
+input_movies_csv = os.path.join(base_dir, "datasets/MovieLens", "movies.csv")
 
-split_ratings_dataset(
-  input_csv=input_csv,
-  output_dir=output_dir,
-  chunksize=CHUNKSIZE,
-  test_size= TEST_SIZE
+output_dir = os.path.join(base_dir, "datasets/mmr_data")
+output_dir_rating = os.path.join(base_dir, "datasets/MovieLens")
+
+
+ratings_df = standardize_csv(
+    input_csv=input_rating_csv,
+    output_csv=os.path.join(output_dir_rating, f"ratings_{CHUNKSIZE}_.csv"),
+    col_mapping={"userId": "userId", "movieId": "itemId", "rating": "rating"},
+    drop_columns=["timestamp"],
+    nrows = CHUNKSIZE,
+)
+
+
+# standardize_csv(
+#     input_csv=input_movies_csv,
+#     output_csv=os.path.join(output_dir_rating, f"movies.csv"),
+#     col_mapping={"movieId": "itemId", "title": "title", "genres": "genres"},
+# )
+
+
+split_ratings(
+    ratings_df,
+    output_dir=output_dir,
+    dataset_name="movies",
+    test_size=0.2,
+    val_size=0.2,
+    chunksize = CHUNKSIZE,
 )
 
 
 
-
-# ratings = pd.read_csv(ratings_file_path, nrows=chunksize)
-
-
-# train_list = []
-# test_list = []
-
-
-# for user_id, group in ratings.groupby('userId'):
-#   train_ratings, test_ratings = train_test_split(group, test_size=test_size, random_state=42)
-#   train_list.append(train_ratings)
-#   test_list.append(test_ratings)
-
-
-
-# train_df = pd.concat(train_list).reset_index(drop=True)
-# test_df = pd.concat(test_list).reset_index(drop=True)  
-
-# # build full file path
-# base_dir = os.path.dirname(os.path.abspath(__file__))
-# train_file_path = os.path.join(base_dir,"datasets", "ratings_train.csv")
-# test_file_path = os.path.join(base_dir,"datasets", "ratings_test.csv" )
-
-
-# train_df.to_csv(train_file_path, index=False)
-# test_df.to_csv(test_file_path, index=False)
-
-# print(f"Test and Train dataset genreated, with {chunksize} number of rows")
