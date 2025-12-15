4d00be89980884651c45134defb2b25a13b2bbe5
Kasia-eto <143899969+Kasia-eto@users.noreply.github.com>
2025-12-15 11:01:51 +0100

    eval updated code

--- src/DPP/DPP.py
+++ src/DPP/DPP.py
@@ -177,7 +177,7 @@ def process_dpp(user_id, user_idx, dpp_indices, item_id, feature_map,
 
 # Run DPP for all users
 
-def get_recommendations_for_dpp(dpp_model, movie_user_rating, item_id, genre_map,
+def get_recommendations_for_dpp(dpp_model, movie_user_rating, item_ids, genre_map,
                                 predicted_ratings, item_to_id, top_k, top_n,output_dir, similarity_type):
 
     results = []
@@ -200,7 +200,7 @@ def get_recommendations_for_dpp(dpp_model, movie_user_rating, item_id, genre_map
             user_id=user_id,
             user_idx=user_idx,
             dpp_indices=dpp_indices,
-            item_names = item_id,
+            item_names = item_ids,
             feature_map=genre_map,
             predicted_ratings=predicted_ratings,
             dpp_recommendations_list=results,
