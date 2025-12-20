dc63522f48df1bf12cc3a0c312b4cd1503c9ebde
Kasia-eto <143899969+Kasia-eto@users.noreply.github.com>
2025-12-20 16:50:42 +0100

    NN 100k books - MLP w. BPR data pipeline - 1 layers

--- src/DPP/DPP_Pipeline_NN.py
+++ src/DPP/DPP_Pipeline_NN.py
@@ -397,42 +397,42 @@ if __name__ == "__main__":
 
 
     # BOOKS - MLP with BPR
-    MLPwithBPR_gb100k_3layer_nn_candidate_list_path =  os.path.join(base_dir, "../datasets/dpp_data/MLPwithBPR/gb100k", "Books100K_NNgenres_ThreeLayers_embed64_lr0001_batch64_ranked_final.csv")
+    MLPwithBPR_gb100k_3layer_nn_candidate_list_path =  os.path.join(base_dir, "../datasets/dpp_data/MLPwithBPR", "Books100K_NNgenres_ThreeLayers_embed64_lr0001_batch64_ranked_final.csv")
     MLPwithBPR_gb100k_3layer = "gb100k_NNgenres_ThreeLayers_embed64_lr0001_batch64"
     MLPwithBPR_books_output_dir = os.path.join(base_dir,f"../datasets/dpp_data/books_NN_MLPwithBPR")
-
-    run_book_id = generate_run_id()
-    run_test_pipeline(
-         run_id = run_book_id,
-         nn_candidates_csv = MLPwithBPR_gb100k_3layer_nn_candidate_list_path ,
-         train_ratings_path=books_ratings_train_path,
-         item_path=books_item_file_path,
-         output_dir=MLPwithBPR_books_output_dir,
-         dataset=MLPwithBPR_gb100k_3layer,
-         top_n=TOP_N,
-         top_k=TOP_K,
-         chunksize=CHUNK_SIZE_100K,
-         best_lambda_cosine = books_100k_cos_lambda,
-         best_lambda_jaccard = books_100k_jac_lambda,
-     )
-
-
-
-
-    MLPwithBPR_gb100k_1layer_nn_candidate_list_path =  os.path.join(base_dir, "../datasets/dpp_data/MLPwithBP/gb100k", "Books100K_RecommendBPRnn_OneLayer_embed64_lr00003_batch128_ranked_final.csv")
-    MLPwithBPR_gb100k_1layer = "gb100k_OneLayer_embed64_lr00003_batch128"
 
     # run_book_id = generate_run_id()
     # run_test_pipeline(
     #     run_id = run_book_id,
-    #     nn_candidates_csv = MLPwithBPR_gb100k_1layer_nn_candidate_list_path ,
+    #     nn_candidates_csv = MLPwithBPR_gb100k_3layer_nn_candidate_list_path ,
     #     train_ratings_path=books_ratings_train_path,
     #     item_path=books_item_file_path,
     #     output_dir=MLPwithBPR_books_output_dir,
-    #     dataset=MLPwithBPR_gb100k_1layer,
+    #     dataset=MLPwithBPR_gb100k_3layer,
     #     top_n=TOP_N,
     #     top_k=TOP_K,
     #     chunksize=CHUNK_SIZE_100K,
     #     best_lambda_cosine = books_100k_cos_lambda,
     #     best_lambda_jaccard = books_100k_jac_lambda,
-    #)
+    # )
+
+
+
+
+    MLPwithBPR_gb100k_1layer_nn_candidate_list_path =  os.path.join(base_dir, "../datasets/dpp_data/MLPwithBPR/gb100k", "Books100K_RecommendBPRnn_OneLayer_embed64_lr00003_batch128_ranked_final.csv")
+    MLPwithBPR_gb100k_1layer = "gb100k_OneLayer_embed64_lr00003_batch128"
+
+    run_book_id = generate_run_id()
+    run_test_pipeline(
+         run_id = run_book_id,
+         nn_candidates_csv = MLPwithBPR_gb100k_1layer_nn_candidate_list_path ,
+         train_ratings_path=books_ratings_train_path,
+         item_path=books_item_file_path,
+         output_dir=MLPwithBPR_books_output_dir,
+         dataset=MLPwithBPR_gb100k_1layer,
+         top_n=TOP_N,
+         top_k=TOP_K,
+         chunksize=CHUNK_SIZE_100K,
+         best_lambda_cosine = books_100k_cos_lambda,
+         best_lambda_jaccard = books_100k_jac_lambda,
+    )
